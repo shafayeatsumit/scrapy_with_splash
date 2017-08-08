2fb8c0cd00dd28d150dcc9b38b3bae8bd2125c06
shafayeatsumit <shafayeatkabir@gmail.com>
2017-08-08 23:33:35 +0600

    modification in lua script, grabbing page inner content

--- scrapy_splash_exaple/spiders/whoscored_spider.py
+++ scrapy_splash_exaple/spiders/whoscored_spider.py
@@ -1,13 +1,31 @@
+# -*- coding: utf-8 -*-
+
 import scrapy
 import json
 from scrapy_splash import SplashRequest
 
 script = """
 function main(splash)
-    splash:set_result_content_type("text/html; charset=utf-8")
     assert(splash:go(splash.args.url))
-    splash:wait(5)
-    return splash:html()
+    local get_info = splash:jsfunc([[
+    function () {
+    var course_detail = document.getElementById('note').innerHTML;
+    return course_detail;
+    }
+    ]])    
+    function wait_for(splash, condition)
+        while not condition() do
+            splash:wait(0.05)
+        end
+    end
+
+    wait_for(splash, function()
+        return splash:evaljs("document.getElementById('note') !== null")
+    end)
+
+    return {
+        data = get_info()
+    }
 end
 """
 
@@ -24,11 +42,14 @@ class WhoscoredspiderSpider(scrapy.Spider):
             yield SplashRequest(url, self.parse,
                 endpoint='execute',
                 args={
-                    'lua_source': script
+                    'lua_source': script,
+                    'timeout': 90
                 }
             )
 
     def parse(self, response):
-        print("parsing from here ++++++")
-        print ("response body=====>",response.body)
+        rs = response.body.decode('unicode_escape')
+        print(type(rs))
+        print (rs.replace("\n",''))
+    
 
